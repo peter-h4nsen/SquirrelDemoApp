d2ff12107f785d1e16327f9a7aad6bccd2471787
peter-h4nsen <peter-h4nsen@users.noreply.github.com>
2015-12-15 22:38:16 +0100

    Added project

---
+++ SquirrelDemoApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SquirrelDemoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnUpdateApp(object sender, RoutedEventArgs e)
+        {
+            using (var mgr = new UpdateManager(@"c:\Code\Temp\SquirrelDeployFiles\Releases\"))
+            {
+                var releaseEntry = await mgr.UpdateApp();
+            }
+        }
+    }
+}
